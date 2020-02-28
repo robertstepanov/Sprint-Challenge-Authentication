9b97a2a8f419fc19e10d7c6d76bca9bdad81d3b4
Robert Stepanov <robertdstepanov@gmail.com>
2020-02-28 12:22:02 -0500

    Hash and Token authentication done, just testing left

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const helmet = require('helmet');
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const usersRouter = require('../users/users-router');
 
 const server = express();
 
@@ -13,6 +14,25 @@ server.use(cors());
 server.use(express.json());
 
 server.use('/api/auth', authRouter);
+server.use('/api/users', authenticate, usersRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
 
+server.get('/', ( req, res ) => {
+  res.send("It's alive");
+})
+
 module.exports = server;
+
+// function checkRole(role) {
+//   return (req, res, next) => {
+//     if (
+//       req.decodedToken &&
+//       req.decodedToken.role &&
+//       req.decodedToken.role.toLowerCase() === role
+//     ) {
+//       next();
+//     } else {
+//       res.status(403).json({ you: "shall not pass!" });
+//     }
+//   };
+// }
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,60 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const Users = require('../users/users-model');
+const { jwtSecret } = require('../config/secrets');
 
 router.post('/register', (req, res) => {
   // implement registration
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+
+  Users.add(user)
+  .then(saved => {
+    res.status(201).json(saved)
+  })
+  .catch(error => {
+    res.status(500).json(error);
+  })
 });
 
 router.post('/login', (req, res) => {
   // implement login
+  let { username, password } = req.body;
+
+  Users.findBy( { username })
+  .first()
+  .then(user => {
+    if (user && bcrypt.compareSync(password, user.password)) {
+      const token = generateToken(user);
+
+      res.status(200).json({ message: `Welcome ${user.username}!`,
+      token,
+    })
+  } else {
+    res.status(401).json({ message: 'Invalid Credentials'});
+  }
+})
+.catch(error => {
+  console.log('ERROR: ', error);
+  res.status(500).json({ error: '/login error' })
+})
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username,
+    role: user.role || 'user',
+  };
+
+  const options = {
+    expiresIn: '2hr',
+  };
+
+  return jwt.sign(payload, jwtSecret, options);
+}
 
 module.exports = router;
